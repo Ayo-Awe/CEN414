e3c0e37a4b7102da15684779e3a95b30f1940954
Awe Ayomidipupo <pupoawe@gmail.com>
2023-01-26 11:26:13 +0100

    add other code

---
+++ Week-8/app.py
@@ -0,0 +1,5 @@
+def yam():
+    print("I love yam")
+ls = "heo"
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
